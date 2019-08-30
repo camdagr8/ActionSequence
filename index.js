2a6ced81f38ecd745cf21e84b9c1ded6a15cf9fc
John Dillick <john@dillick.us>
2019-08-29 20:15:18 -0400

    Fix break syntax on error. Patch 1.0.6

--- index.js
+++ index.js
@@ -9,17 +9,19 @@ module.exports = ({
     return new Promise(async (resolve, reject) => {
         let result;
         for (action in actions) {
-            result = await actions[action]({
-                ...options,
-                action,
-                result,
-            }).then(result => {
-                success.push(result);
-                return result;
-            }).catch(error => {
+            try {
+                result = await actions[action]({
+                    ...options,
+                    action,
+                    result,
+                }).then(result => {
+                    success.push(result);
+                    return result;
+                })
+            } catch(error) {
                 errors.push(error);
                 break;
-            });
+            }
         }
 
         if (errors.length > 0) {
