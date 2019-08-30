3463f750ed6241d18a62d5e2830cc6ecb5849d43
John Dillick <john@dillick.us>
2019-08-29 20:06:27 -0400

    Break on promise rejection in action.

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ module.exports = ({
                 success.push(result);
             }).catch(error => {
                 errors.push(error);
+                break;
             });
         }
 
